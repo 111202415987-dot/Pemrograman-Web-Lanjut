60057ec2987697632a2c1c27d86f64f02d8f47ba
VASYA CITRA <111202415987@mhs.dinus.ac.id>
2026-04-21 22:29:23 +0700

    Session 1

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -180,7 +180,7 @@
             <img src=" <?= base_url()?>NiceAdmin/ <?= base_url()?>NiceAdmin/assets
 
 /img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2"><?= session()->get('username'); ?> (<?= session()->get('role'); ?>)</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
@@ -223,7 +223,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
+              <a class="dropdown-item d-flex align-items-center" href="logout">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
